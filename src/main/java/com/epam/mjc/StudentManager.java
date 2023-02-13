90e06bc326e42e79142f8edd670a7d6c5ccab9b2
Jahongir2109 <jahongirachilov004@gmail.com>
2023-02-13 12:03:06 +0500

    git coomit all

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,11 +1,16 @@
 package com.epam.mjc;
 
 
-public class StudentManager {
+public class StudentManager  {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
+   Student student= Student.getValueOf(studentID);
+    if (student==null){
+      throw new StudentNotFoundException(
+              "Could not find student with ID " + studentID);
+    }
     return Student.getValueOf(studentID);
   }
 
---
+++ src/main/java/com/epam/mjc/StudentManagerTest.java
@@ -0,0 +1,9 @@
+package com.epam.mjc;
+
+public class StudentManagerTest extends StudentManager {
+    @Override
+    public Student find(long studentID) {
+        return super.find(studentID);
+    }
+
+}
